db63d1c7cbd2fc87252a587bf4e0ffe7c319b12a
Miguel Fernandez <miguel.fernandez@oregonstate.edu>
2019-01-08 13:31:17 -0800

    Adding mock for advisor student summary page.

---
+++ BennyAdvisor/Pages/Advisor/Student/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using BennyAdvisor.Models;
+
+namespace BennyAdvisor.Pages.Advisor.Student
+{
+    public class IndexModel : PageModel
+    {
+        public StudentModel Student { get; set; }
+
+        public void OnGet()
+        {
+            Student = new StudentModel()
+            {
+                Level = "Graduate",
+                ImagePath = "/images/test.jpg",
+                FirstName = "Roger",
+                LastName = "Montgomery",
+                DegreeName = "ASSOC OF SCIENCE",
+                DegreeCode = "AS1"
+            };
+        }
+    }
+}
--- BennyAdvisor/wwwroot/js/site.js
+++ BennyAdvisor/wwwroot/js/site.js
@@ -2,3 +2,63 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+
+
+//
+// Tab functionality.
+//
+
+function tabClick(e) {
+    tabShow($(e).closest("[data-tab^='tab']").attr("data-tab"));
+}
+
+function tabShow(tabId) {
+    // Call the initialize handler for the tab so that the data get loaded.
+    var initHandler = tabId + "Init";
+    eval("if (typeof " + initHandler + " !== 'undefined')" + initHandler + "()");
+
+    // Show the tab view and select the tab side menu.
+    $(".tab-container").attr("data-tab", tabId);
+    $(".nav-item").removeClass("active");
+    $(".nav-item[data-tab=" + tabId + "]").addClass("active");
+    // Set the url hash so that the url shows the active tab.
+    window.location.hash = tabId;
+}
+
+//
+// Student summary tab.
+//
+
+function tabSummaryInit() {
+    var tmpl = $.templates("#termCardTmpl");
+
+    // TODO: Load data by ajax get.
+    var data = {
+        bgClass: "bg-info",
+        readonly: true,
+        termCode: "20193",
+        termTitle: "2019 Winter",
+        courses: [{
+            code: "X",
+            title: "Sample Course",
+            grade: "4.0",
+            credit: 3
+        },
+        {
+            code: "X2",
+            title: "Another Course",
+            grade: "4.0",
+            credit: 3
+        },
+        {
+            code: "X3",
+            title: "One More Course",
+            grade: "4.0",
+            credit: 3
+        }]
+    };
+
+    $("#tabSummaryContainer").html($(tmpl.render(data)));
+}
+
